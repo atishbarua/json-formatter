8f5f3727a381ee85f7c653f41971271871cbb519
barua.ati <barua.ati@gmail.com>
2025-12-02 11:30:27 +0600

    Fix: redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -12,7 +12,9 @@ function Router() {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/admin" component={Admin} />
-      <Route component={NotFound} />
+      {/* <Route component={NotFound} /> */}
+      {/* Catch-all redirect */}
+      <Redirect to="/" />
     </Switch>
   );
 }
